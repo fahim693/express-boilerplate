6730924d0ac81fd5f84f407006ae1bc8ac2b22f3
Fahim Chowdhury <fahimchowdhury@Fahims-MacBook-Pro.local>
2020-06-15 10:04:39 +0600

    basic project

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var cors = require('cors')
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+const port = process.env.PORT;
+
+app.use(cors())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(
+    `Server ready at http://localhost:${port}`
+  );
+});
+
+
+module.exports = app;
